c7a3784b51e6ad6c686a3ab5051779580725b022
sh1hab <shihab.gsc10@gmail.com>
2021-08-03 23:33:55 +0600

    localization

---
+++ resources/lang/en/app.php
@@ -0,0 +1,74 @@
+<?php
+
+return [
+
+    'comment' => [
+        'already_created' => 'Comment already created!',
+        'create' => [
+            'success' => 'Comment created successfully',
+        ],
+        'update' => [
+            'success' => 'Comment updated successfully',
+        ],
+        'cancel' => [
+            'success' => 'Comment cancelled successfully',
+        ]
+    ],
+    'create' => [
+        'success' => 'Successfully saved data!',
+    ],
+    'delete' => [
+        'success' => 'Successfully deleted data!',
+    ],
+    'email' => [
+        'not_valid' => 'Your email isn\'t valid',
+        'mismatch' => 'Miss match given email address with token email address!'
+    ],
+    'failed' => 'Failed!',
+    'get' => [
+        'success' => 'Successfully retrieved data!',
+    ],
+    'login' => [
+        'success' => 'successfully logged in'
+    ],
+    'logout' => [
+        'success' => 'successfully logged out'
+    ],
+    'login-validation' => [
+        'email' => 'The :attribute must be a valid email address.'
+    ],
+
+    'password' => [
+        'not_valid' => 'Your password isn\'t valid',
+    ],
+    'reset_password' => [
+        'success' => 'Reset password success'
+    ],
+    'success' => 'Success!',
+    'token' => [
+        'not_valid' => 'Not valid token!',
+    ],
+    'user' => [
+        'not_found' => 'This user is not found',
+        'not_valid' => 'Not valid User!',
+        'in_active' => 'This user is inactive. Please contact to the admin',
+        'registered' => 'This user already registered!',
+        'register' => [
+            'success' => 'User successfully registered!',
+            'failed' => 'User register failed!'
+        ],
+        'verify' => [
+            'success' => 'User successfully verified!',
+            'failed' => 'User verified failed!'
+        ],
+        'role' => [
+            'forbidden' => 'You have no permission for the action'
+        ],
+    ],
+    'update' => [
+        'success' => 'Successfully updated data!',
+    ],
+    'validation' => [
+        'failed' => 'Validation failed'
+    ]
+];
